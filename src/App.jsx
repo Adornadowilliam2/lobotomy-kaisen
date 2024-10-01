2c6a9c696974373c5138a8e82cb32671a72c0aff
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-10-01 16:09:05 +0800

    make it much cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { data } from "./index.js";
 
 function App() {
   return (
@@ -6,90 +7,12 @@ function App() {
       <div>
         <h1>These were truly our lobotomy kaisen 😭🙏</h1>
         <div className="card-container">
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.QG3METzSviNGrWfoOWvjMgAAAA&w=225&h=277&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="nah i win"
-            />
-            <h2>Nah, I'd Win</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.T1revz3MNTEBefpi_dikTgHaHE&w=255&h=244&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="fraudkuna"
-            />
-            <h2>FraudKuna</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th/id/OIP.O4f7Q3jUA2UrbI2-0IIamQHaMU?w=119&h=199&c=7&r=0&o=5&pid=1.7"
-              alt="waffle kashimo"
-            />
-            <h2>Waffle Kashimo</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.E_LAjDzmW2R-zlkpKRGJlAHaI4&w=228&h=273&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="bushcamper yuta"
-            />
-            <h2>Bushcamper Yuta</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://www.bing.com/th?id=OIP.HWhVPVoKaqkHrjt5oIMK1wHaIx&w=150&h=178&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="adapting to legal system"
-            />
-            <h2>Adapting to Legal System</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.Ky965jVN_tP9oSjF9JgmhQHaIG&w=239&h=261&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="Femboy kashimo"
-            />
-            <h2>Femboy Kashimo</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.k46nrQouvjy0n-tv6HBlEgHaI5&w=228&h=274&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="jogoat"
-            />
-            <h2>Jogoat</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://www.bing.com/th?id=OIP.Uouh--rUL5tJyJKJ2hR0NwHaHa&w=103&h=103&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="kenjaku backshot"
-            />
-            <h2>Kenjaku Backshot</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.LHqiXI85rpZaR59BHsxIRgAAAA&w=224&h=277&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="heian era technique"
-            />
-            <h2>Heian Era Technique</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.YnDH8T9iLNvYytAP-3S8kQHaKC&w=214&h=290&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="Bumgumi"
-            />
-            <h2>Bumgumi</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th?id=OIP.ADJ6hO09HhY4uBg0hVu8igHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2"
-              alt="sukuna cycle"
-            />
-            <h2>Sukuna Cycle</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://th.bing.com/th/id/OIP.lra9V6QTKQkSCJUxmASAPQHaHC?w=158&h=180&c=7&r=0&o=5&pid=1.7"
-              alt="poukuna"
-            />
-            <h2>Poukuna</h2>
-          </div>
+          {data.map((item) => (
+            <div className="card" key={item.id}>
+              <img src={item.image} alt={item.name} />
+              <h2>{item.name}</h2>
+            </div>
+          ))}
         </div>
       </div>
     </>
---
+++ src/index.js
@@ -0,0 +1,74 @@
+export const data = [
+  {
+    id: 1,
+    name: "Nah, I'd Win",
+    image:
+      "https://th.bing.com/th?id=OIP.QG3METzSviNGrWfoOWvjMgAAAA&w=225&h=277&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 2,
+    name: "FraudKuna",
+    image:
+      "https://th.bing.com/th?id=OIP.T1revz3MNTEBefpi_dikTgHaHE&w=255&h=244&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 3,
+    name: "Waffle Kashimo",
+    image:
+      "https://th.bing.com/th/id/OIP.O4f7Q3jUA2UrbI2-0IIamQHaMU?w=119&h=199&c=7&r=0&o=5&pid=1.7",
+  },
+  {
+    id: 4,
+    name: "Bushcamper Yuta",
+    image:
+      "https://th.bing.com/th?id=OIP.E_LAjDzmW2R-zlkpKRGJlAHaI4&w=228&h=273&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 5,
+    name: "Adding to Legal System",
+    image:
+      "https://www.bing.com/th?id=OIP.HWhVPVoKaqkHrjt5oIMK1wHaIx&w=150&h=178&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 6,
+    name: "Femboy Kashimo",
+    image:
+      "https://th.bing.com/th?id=OIP.Ky965jVN_tP9oSjF9JgmhQHaIG&w=239&h=261&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 7,
+    name: "Jogoat",
+    image:
+      "https://th.bing.com/th?id=OIP.k46nrQouvjy0n-tv6HBlEgHaI5&w=228&h=274&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 8,
+    name: "Kenjaku Backshot",
+    image:
+      "https://www.bing.com/th?id=OIP.Uouh--rUL5tJyJKJ2hR0NwHaHa&w=103&h=103&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 9,
+    name: "Heian Era Technique",
+    image:
+      "https://th.bing.com/th?id=OIP.LHqiXI85rpZaR59BHsxIRgAAAA&w=224&h=277&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 10,
+    name: "Bumgumi",
+    image:
+      "https://th.bing.com/th?id=OIP.YnDH8T9iLNvYytAP-3S8kQHaKC&w=214&h=290&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 11,
+    name: "Sukuna Cycle",
+    image:
+      "https://th.bing.com/th?id=OIP.ADJ6hO09HhY4uBg0hVu8igHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2",
+  },
+  {
+    id: 12,
+    name: "Poukuna",
+    image:
+      "https://th.bing.com/th/id/OIP.lra9V6QTKQkSCJUxmASAPQHaHC?w=158&h=180&c=7&r=0&o=5&pid=1.7",
+  },
+];
